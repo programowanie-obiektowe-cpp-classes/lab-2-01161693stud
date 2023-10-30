663443bc78418a0e01bf470e594c831c72584051
01161693stud <149065388+01161693stud@users.noreply.github.com>
2023-10-30 22:00:37 +0100

    Update ResourceManager 1
    test nr1

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -2,7 +2,30 @@
 
 #include "Resource.hpp"
 
-class ResourceManager
-{
-    // Twoja implementacja tutaj
+class ResourceManager {
+public:
+    ResourceManager() {
+        resource_ = Resource(); 
+    }
+
+    double get() {
+        return resource_.get();
+    }
+
+    ResourceManager(const ResourceManager& other) {
+        resource_ = other.resource_;
+    }
+
+    ResourceManager& operator=(const ResourceManager& other) {
+        if (this != &other) {
+            resource_ = other.resource_;
+        }
+        return *this;
+    }
+
+    ~ResourceManager() {
+    }
+
+private:
+    Resource resource_;
 };
